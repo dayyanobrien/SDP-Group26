4b66c2111e1fc329fb3927a7100d56493c96b9d8
Lance Bao <79973629+LanceBao0313@users.noreply.github.com>
2022-04-05 22:50:01 +0100

    Add files via upload

--- rpi-kivy-screen/test.py
+++ rpi-kivy-screen/test.py
@@ -12,22 +12,33 @@ from kivy.clock import Clock
 from kivy.graphics import Color, Rectangle
 from kivy.uix.popup import Popup
 
+import RPi.GPIO as GPIO
+
 import rospy
 import std_msgs
 from std_msgs.msg import String
 
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+
 def callback(msg):
 	if msg.data == 'going home':
 		print('going home')
+		GPIO.output(18,GPIO.HIGH)
 		#App.get_running_app().root.current = "returning"
 	if msg.data  == 'going to table':
 		print('going to table')
+		GPIO.output(18,GPIO.HIGH)
 		#App.get_running_app().root.current = "delivering"
 	if msg.data == 'Reached home':
+		GPIO.output(18,GPIO.LOW)
 		App.get_running_app().root.current = "kitchen"
 	if msg.data == 'Reached table':
+		GPIO.output(18,GPIO.LOW)
 		App.get_running_app().root.current = "table"
 	if msg.data == 'Stuck':
+		PIO.output(18,GPIO.LOW)
 		App.get_running_app().root.current = "warn"
 		
 pub = rospy.Publisher('chatter', String, queue_size=10)
@@ -35,7 +46,6 @@ sub = rospy.Subscriber('navi_status', String, callback, queue_size = 10)
 
 rospy.init_node('talker', anonymous=True)
 
-		
 #Define our different screens
 class Kitchen(Screen):
 	def table_pressed(self, instance):
@@ -52,10 +62,11 @@ class Kitchen(Screen):
 		if len(tables) == 1:
 			tables[0].state = 'normal'
 			self.manager.current = "delivering"
-			pub.publish(str(tables[0].text))
+			pub.publish(str(tables[0].text.split(' ')[1]))
 			tables = []
+			GPIO.output(18, GPIO.HIGH)
 		else:
-			content = Button(text='Only select one table.')
+			content = Button(text='Error: Only select one table.', font_size = 20, color = (1.0, 0.0, 0.0, 1.0), background_color = (0.0, 0.0, 0.0, 0.0))
 			popup = Popup(title = 'Warning.', content=content, auto_dismiss=False, size_hint=(None, None), size=(500, 200))
 
 			# bind the on_press event of the button to the dismiss function
@@ -80,8 +91,9 @@ class Kitchen2(Screen):
 		if len(tables) == 1:
 			tables[0].state = 'normal'
 			self.manager.current = "delivering"
-			pub.publish(str(tables[0].text))
+			pub.publish(str(tables[0].text.split(' ')[1]))
 			tables = []
+			GPIO.output(18,GPIO.HIGH)
 		else:
 			content = Button(text='Only select one table.')
 			popup = Popup(title = 'Warning.', content=content, auto_dismiss=False, size_hint=(None, None), size=(500, 200))
@@ -98,15 +110,22 @@ class Table(Screen):
 	def returnHome(self, instance):
 		self.manager.current = "returning"
 		pub.publish('Recieved')
+		GPIO.output(18,GPIO.HIGH)
 
 class Delivering(Screen):
-	pass
-
+	def openDoor(self, instance):
+		print('low')
+		GPIO.output(18,GPIO.LOW)
+		
 class Returning(Screen):
-	pass
+	def openDoor(self, instance):
+		print('low')
+		GPIO.output(18,GPIO.LOW)
 
 class Warn(Screen):
-	pass
+	def openDoor(self, instance):
+		print('low')
+		GPIO.output(18,GPIO.LOW)
 	
 class WindowManager(ScreenManager):
 	pass
@@ -120,6 +139,7 @@ class AwesomeApp(App):
 		return kv
 		
 if __name__ == '__main__':
+	print(4)
 	global tables
 	tables = []
 	AwesomeApp().run()
